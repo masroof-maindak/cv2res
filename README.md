f2d1576f508a719ab440e330a7e2bbb59fef24ae
masroof-maindak <mujtaba.asim.amin@gmail.com>
2026-05-19 22:48:13 +0500

    feat: format + compilation support
    docs: add cli args to readme

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Unreasonably straightforward Rust tool built because manually tweaking my
 `resume.typ` depending on the job application was too arduous.
 
 This program reads a `toml` file w/ pre-set fields, and fills them into a Typst
-resume template, eventually outputting it,
+resume template, eventually outputting it, formatting it (w/ `typstyle`), and
+compiling it (w/ `typst`).
 
 ## Setup
 
@@ -20,7 +21,13 @@ cargo install toml2cv
 ## Usage
 
 ```bash
-# TODO: explain cli flags
+# Assuming `cv2.toml` exists in the PWD; produces `output.typ`
+toml2cv
+
+# Run in another directory, and format + build Typst output file
+toml2cv -d ~/Desktop/Resume/ --format --compile
+
+# See `toml2cv -h` for more help
 ```
 
 ## Configuration
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, bail};
 use clap::Parser;
 use std::fs;
+use std::path::Path;
+use std::process::Command;
 use tera::{self, Tera};
 
 use crate::structs::CV;
@@ -11,24 +13,54 @@ const DEFAULT_IN_FNAME: &str = "2cv.toml";
 const DEFAULT_OUT_FNAME: &str = "output.typ";
 const DEFAULT_DIR: &str = ".";
 const DEFAULT_TMPLT_NAME: &str = "cv.typ";
+const FMT_BIN: &str = "typstyle";
+const TYPST_BIN: &str = "typst";
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, default_value = DEFAULT_IN_FNAME)]
+    #[arg(short, long, default_value = DEFAULT_IN_FNAME, help = "input filename")]
     input: String,
 
-    #[arg(short, long, default_value = DEFAULT_OUT_FNAME)]
+    #[arg(short, long, default_value = DEFAULT_OUT_FNAME, help = "output filename")]
     output: String,
 
-    #[arg(short, long, default_value = DEFAULT_DIR)]
+    #[arg(short, long, default_value = DEFAULT_DIR, help = "directory to look for input file in")]
     dir: String,
 
-    #[arg(short, long, default_value_t = false)]
-    print: bool,
-
-    #[arg(short, long, default_value_t = false)]
+    #[arg(
+        short,
+        long,
+        default_value_t = false,
+        help = "whether to anonymise some personal info"
+    )]
     anon: bool,
+
+    #[arg(
+        short,
+        long,
+        default_value_t = false,
+        help = "run formatter after generating output"
+    )]
+    format: bool,
+
+    #[arg(
+        short,
+        long,
+        default_value_t = false,
+        help = "compile w/ typst after generating output"
+    )]
+    compile: bool,
+}
+
+fn check_if_in_path(bin: &str) -> Result<bool> {
+    match std::env::var("PATH") {
+        Ok(paths) => Ok(paths
+            .split(":")
+            .map(|p| format!("{}/{}", p, bin))
+            .any(|p| Path::new(&p).exists())),
+        Err(e) => bail!(format!("Failed to find binary {bin} in PATH: {e}.")),
+    }
 }
 
 fn parse_cfg_file(path: &str) -> Result<CV> {
@@ -55,14 +87,40 @@ fn main() -> Result<()> {
     tera.add_raw_template(DEFAULT_TMPLT_NAME, tmplt)?;
     let output = tera.render(DEFAULT_TMPLT_NAME, &tera::Context::from_serialize(&cv)?)?;
 
-    if args.print {
-        println!("{output}");
-    }
-
-    std::fs::write(args.output, output)
+    std::fs::write(&args.output, output)
         .with_context(|| "Failed to dump output to file.".to_string())?;
 
-    // TODO: run a typst formatter and `typst compile fname.typ` too
+    if args.format {
+        let exists =
+            check_if_in_path(FMT_BIN).with_context(|| "Failed to search PATH".to_string())?;
+
+        if exists {
+            Command::new(FMT_BIN)
+                .args([&args.output, "--wrap-text", "--inplace"])
+                .spawn()
+                .expect("Formatter failed to start.");
+
+            // TODO: check output
+        } else {
+            println!("Failed to find `{FMT_BIN}` in PATH; skipping formatting.")
+        }
+    }
+
+    if args.compile {
+        let exists =
+            check_if_in_path(TYPST_BIN).with_context(|| "Failed to search PATH".to_string())?;
+
+        if exists {
+            Command::new(TYPST_BIN)
+                .args(["compile", &args.output])
+                .spawn()
+                .expect("Typst failed to start.");
+
+            // TODO: check output
+        } else {
+            println!("Failed to find `{TYPST_BIN}` in PATH; skipping compiling.")
+        }
+    }
 
     Ok(())
 }
